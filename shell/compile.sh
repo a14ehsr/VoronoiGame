ac545d0cc112986b3de64ad2ceb3cf34f1e06814
a14ehsr <a14.ehsr@g.chuo-u.ac.jp>
2018-11-21 11:41:36 +0900

    8近傍AIの実装

--- shell/compile.sh
+++ shell/compile.sh
@@ -8,6 +8,7 @@ ${command} ${options} ac/a14ehsr/platform/VoronoiGame.java
 ${command} ${options} ac/a14ehsr/sample_ai/P_Random.java
 ${command} ${options} ac/a14ehsr/sample_ai/P_Max.java
 ${command} ${options} ac/a14ehsr/sample_ai/P_4Neighbours.java
+${command} ${options} ac/a14ehsr/sample_ai/P_8Neighbours.java
 ${command} ${options} ac/a14ehsr/sample_ai/P_Chaise.java
 
 cd ../../
